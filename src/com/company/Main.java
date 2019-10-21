eff6fc5059eb1016b48547c13ef59968b3104077
pierzbarry <pmbarry@asu.edu>
2019-10-21 16:31:25 -0400

    new commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double myNumber = 2;
+	double myOtherNumber = 1.7938;
+
+        System.out.println(myNumber);
+        System.out.println(myOtherNumber);
+
+        System.out.println(myNumber + myOtherNumber);
+    }
+}
